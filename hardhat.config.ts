6ae207e791a198f02171414613e3360a14e867ab
Anthony Lee <antwonlee@gmail.com>
2022-06-21 23:28:42 -0500

    test: Add simple test script to test hh test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,10 +25,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    hardhat: {
+      allowUnlimitedContractSize: true,
     },
   },
   gasReporter: {
--- test/daal.ts
+++ test/daal.ts
@@ -1,6 +1,22 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Daal", function () {
-  it("Should mint NFT", async function () {});
+describe("DAAL contract", function () {
+  let Token;
+  let daalToken: any;
+  let owner: any;
+  let addr1: any;
+  let addr2: any;
+
+  beforeEach(async function () {
+    Token = await ethers.getContractFactory("DAAL");
+    [owner, addr1, addr2] = await ethers.getSigners();
+    daalToken = await Token.deploy();
+  });
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      expect(await daalToken.owner()).to.equal(owner.address);
+    });
+  });
 });
